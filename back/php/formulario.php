2bd7563fea0d8e5f7f2a6d34230baf5da6d977a5
gabrielle-louise <83481688+gabrielle-louise@users.noreply.github.com>
2021-06-20 14:25:35 -0300

    Update formulario.php
    Colocando o PHP funcionando com o banco de dados

--- back/php/formulario.php
+++ back/php/formulario.php
@@ -1,3 +1,21 @@
 <?php
-echo json_encode($_POST)
+
+	$conexao = mysqli_connect("localhost","root","","projeto3");
+	
+	$result = mysqli_query($conexao,"<query>");
+
+	echo json_encode($_POST);
+	$nome = $_POST["nome"];
+	$sobrenome = $_POST["sobrenome"];
+	$endereco = $_POST["endereco"];	
+	$numeroTelefone = $_POST["numeroTelefone"];
+
+	
+	
+	if($nome != null){
+		$insert = "INSERT INTO cadastro(NOME, SOBRENOME, ENDERECO, NUMERO) 
+		VALUES('$nome','$sobrenome','$endereco','$numeroTelefone')";
+		
+		mysqli_query($conexao,$insert);
+	}
 ?>
